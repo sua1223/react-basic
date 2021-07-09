bb47eaab7a06fcd269eab7c0581c152318d4bbab
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-07-09 15:16:57 +0900

    21/07/08 lecture 4.9~4.12

--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import Habit from './habit';
+import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
     state={
@@ -7,15 +8,38 @@ class Habits extends Component {
             {id: 1, name: 'Reading', count: 0},
             {id: 2, name: 'Running', count: 0},
             {id: 3, name: 'Coding', count: 0},
-        ]
-    }
+        ],
+    };
+
+    handleIncrement = habit => {
+        this.props.onIncrement(habit);
+    };
+    handleDecrement = habit => {
+        this.props.onDecrement(habit);
+    };
+    handleDelete = habit => {
+        this.props.onDelete(habit);
+    };
+
+    handleAdd = name => {
+        this.props.onAdd(name);
+    };
+    
     render() {
         return (
+            <>
+            <HabitAddForm onAdd={this.handleAdd}/>
             <ul>
                 {this.state.habits.map(habit => (
-                    <Habit key={habit.id} habit={habit} />
+                    <Habit 
+                    key={habit.id} 
+                    habit={habit} 
+                    onIncrement={this.handleIncrement}
+                    onDecrement={this.handleDecrement}
+                    onDelete={this.handleDelete}/>
                 ))}
             </ul>
+            </>
         );
     }
 }

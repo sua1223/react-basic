59997ec9e3f9b4b58ccefa6a78d2e0e2531669a6
sua1223 <70890638+sua1223@users.noreply.github.com>
2021-07-12 21:19:02 +0900

    21/07/11 lecture 4.13~4.18

--- habit-tracker/src/components/habit.jsx
+++ habit-tracker/src/components/habit.jsx
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 
-class Habit extends Component {
-   
+class Habit extends PureComponent {
+
     handleIncrement = () =>{
         this.props.onIncrement(this.props.habit);
     };
--- habit-tracker/src/components/habits.jsx
+++ habit-tracker/src/components/habits.jsx
@@ -3,45 +3,27 @@ import Habit from './habit';
 import HabitAddForm from './habitAddForm';
 
 class Habits extends Component {
-    state={
-        habits: [
-            {id: 1, name: 'Reading', count: 0},
-            {id: 2, name: 'Running', count: 0},
-            {id: 3, name: 'Coding', count: 0},
-        ],
-    };
-
-    handleIncrement = habit => {
-        this.props.onIncrement(habit);
-    };
-    handleDecrement = habit => {
-        this.props.onDecrement(habit);
-    };
-    handleDelete = habit => {
-        this.props.onDelete(habit);
-    };
-
-    handleAdd = name => {
-        this.props.onAdd(name);
-    };
-    
-    render() {
-        return (
-            <>
-            <HabitAddForm onAdd={this.handleAdd}/>
-            <ul>
-                {this.state.habits.map(habit => (
-                    <Habit 
-                    key={habit.id} 
-                    habit={habit} 
-                    onIncrement={this.handleIncrement}
-                    onDecrement={this.handleDecrement}
-                    onDelete={this.handleDelete}/>
-                ))}
-            </ul>
-            </>
-        );
-    }
+  render() {
+    return (
+      <div className="habits">
+        <HabitAddForm onAdd={this.props.onAdd} />
+        <ul>
+          {this.props.habits.map(habit => (
+            <Habit
+              key={habit.id}
+              habit={habit}
+              onIncrement={this.props.onIncrement}
+              onDecrement={this.props.onDecrement}
+              onDelete={this.props.onDelete}
+            />
+          ))}
+        </ul>
+        <button className="habits-reset" onClick={this.props.onReset}>
+          Reset All
+        </button>
+      </div>
+    );
+  }
 }
 
-export default Habits
+export default Habits;
